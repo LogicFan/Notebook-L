e1e75f1013185d8b0703d590a003c9e49f6db185
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-03 11:22:59 -0400

    New UI for SettingsPage

--- Notebook-L/Settings/SettingsPage.xaml.cs
+++ Notebook-L/Settings/SettingsPage.xaml.cs
@@ -14,29 +14,15 @@ namespace Notebook_L.Settings
         public SettingsPage()
         {
             this.InitializeComponent();
+            log.Info(String.Format("Padding = {0}, Boderding = {1}", this.ListBox_Menu.Padding, this.ListBox_Menu.BorderThickness));
         }
 
-        private void NavigationView_BackRequested(muxc.NavigationView sender, muxc.NavigationViewBackRequestedEventArgs args)
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
-        }
+            String itemTag = (e.AddedItems[0] as SymbolIcon).Tag as String;
 
-        private void NavigationView_SelectionChanged(muxc.NavigationView sender, muxc.NavigationViewSelectionChangedEventArgs args)
-        {
-            muxc.NavigationViewItem selectedItem = args.SelectedItem as muxc.NavigationViewItem;
-            if (selectedItem != null)
-            {
-                string itemTag = selectedItem.Tag as string;
-                sender.Header = itemTag;
-                Type itemPage = Type.GetType("Notebook_L.Settings." + itemTag);
-                Frame_Settings.Navigate(itemPage);
-            }
-        }
-
-        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
-        {
-            muxc.NavigationView navigationView = sender as muxc.NavigationView;
-            navigationView.SelectedItem = navigationView.MenuItems[0];
+            Type itemPage = Type.GetType("Notebook_L.Settings." + itemTag);
+            Frame_Settings.Navigate(itemPage);
         }
     }
 }
