cf1e170d2c1633c1a4d5f0c4eefed5ab1278e3be
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-05 03:01:16 -0400

    remove unused method

--- Notebook-L/Serializable/Notebook.cs
+++ Notebook-L/Serializable/Notebook.cs
@@ -8,12 +8,5 @@ namespace Notebook_L.Serializable
         public String Name;
         public String Path;
         public String SourceName;
-
-        public Notebook(String name, String path, String sourceName)
-        {
-            this.Name = name;
-            this.Path = path;
-            this.SourceName = sourceName;
-        }
     }
 }
