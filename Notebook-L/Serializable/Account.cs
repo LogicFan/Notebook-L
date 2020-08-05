8ba74dae9e77ec61ef1b63e64a8adee5d6843c16
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-05 00:30:48 -0400

    Add Serializable.Account, Serializable.Notebook

---
+++ Notebook-L/Serializable/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook_L.Serializable
+{
+    [Serializable]
+    struct Account
+    {
+        public enum SourceType
+        {
+            OneDrive,
+            GoogleDrive,
+            iCloudDrive,
+            DropBox
+        }
+
+        public String Name;
+        public String Credential;
+        public SourceType Source;
+    }
+}
---
+++ Notebook-L/Serializable/Notebook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Notebook_L.Serializable
+{
+    [Serializable]
+    struct Notebook
+    {
+        public String Name;
+        public String Path;
+        public String SourceName;
+    }
+}
