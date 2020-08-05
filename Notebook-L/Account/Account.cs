b53a763185eb2f8a0c6ae7d46b6f1a947ae05734
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-05 00:52:49 -0400

    Add class Account.Account

---
+++ Notebook-L/Account/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Notebook_L;
+
+namespace Notebook_L.Account
+{
+    class Account
+    {
+        public Serializable.Account Data { get; set; }
+
+        public String UIName => Data.Name;
+        public String UISource => Data.Source.ToString("G");
+
+        public Account(Serializable.Account data)
+        {
+            this.Data = data;
+        }
+    }
+}
