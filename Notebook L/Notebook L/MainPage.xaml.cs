1c695a215379e9f8b378cfc9c24560d0ad500b46
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-12 01:15:27 -0400

    Add TabView into MainPage

--- Notebook L/Notebook L/MainPage.xaml.cs
+++ Notebook L/Notebook L/MainPage.xaml.cs
@@ -1,30 +1,78 @@
-﻿using System;
+﻿using MetroLog;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Notebook_L
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        // The logger for the class
+        private static readonly ILogger Log = LogManagerFactory.DefaultLogManager.GetLogger<MainPage>();
+        private static readonly ISet<MainPage> Instances = new HashSet<MainPage>();
+
         public MainPage()
         {
+            Log.Info("Create object MainPage");
+
+            Log.Info("Add this into Instances");
+            Instances.Add(this);
+
             this.InitializeComponent();
+
+            ApplicationView.GetForCurrentView().Consolidated += (ApplicationView appView, ApplicationViewConsolidatedEventArgs args) =>
+            {
+                Log.Info("Remove this from Instances");
+                Instances.Remove(this);
+            };
+        }
+
+        private void TabView_Main_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TabView_Main_TabItemsChanged(Microsoft.UI.Xaml.Controls.TabView sender, IVectorChangedEventArgs args)
+        {
+
+        }
+
+        private void TabView_Main_AddTabButtonClick(Microsoft.UI.Xaml.Controls.TabView sender, object args)
+        {
+
+        }
+
+        private void TabView_Main_TabCloseRequested(Microsoft.UI.Xaml.Controls.TabView sender, Microsoft.UI.Xaml.Controls.TabViewTabCloseRequestedEventArgs args)
+        {
+
+        }
+
+        private void TabView_Main_TabDroppedOutside(Microsoft.UI.Xaml.Controls.TabView sender, Microsoft.UI.Xaml.Controls.TabViewTabDroppedOutsideEventArgs args)
+        {
+
+        }
+
+        private void TabView_Main_TabStripDragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TabView_Main_TabStripDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TabView_Main_TabDragStarting(Microsoft.UI.Xaml.Controls.TabView sender, Microsoft.UI.Xaml.Controls.TabViewTabDragStartingEventArgs args)
+        {
+
+        }
+
+        private void Button_Setting_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
