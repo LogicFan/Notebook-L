46d34d81c24f5cd0cddd08f63e4982aafbe84dac
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-13 19:44:34 -0400

    Update AboutPage
    - Add Setting class
    - Version is binding into property in Setting

---
+++ Notebook L/Notebook L/Setting/Setting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook_L.Setting
+{
+    static class Setting
+    {
+        #region NotebookPage
+        #endregion
+
+        #region AboutPage
+        public static String Version => "Alpha v0.1.1";
+        #endregion
+    }
+}
