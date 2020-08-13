51b79e0891b5ed4b8892a599799f112848e2fe69
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-12 20:09:32 -0400

    Add Account struct

---
+++ Notebook L/Notebook L/Serializable/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook_L.Serializable
+{
+    struct Account
+    {
+        public enum SourceType
+        {
+            OneDrive,
+            GoogleDrive,
+            iCloudDrive,
+            Dropbox
+        }
+
+        public String Name;
+        public String Credential;
+        public SourceType Source;
+    }
+}
