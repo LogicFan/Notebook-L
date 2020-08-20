80300a4f8a7ac3a6a91815b1dffefeb07dd7c72b
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-20 00:47:56 -0400

    Add FileSystem
    - Add package Integrative.CrossProtect
    - Implement LocationData

---
+++ Notebook L/Notebook L/FileSystem/LocationData.cs
@@ -0,0 +1,59 @@
+﻿using Integrative.Encryption;
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Runtime.Serialization;
+
+namespace Notebook_L.FileSystem
+{
+    [Serializable]
+    public sealed class LocationData : ISerializable
+    {
+        public enum SourceType
+        {
+            Local,
+            OneDrive
+        }
+
+        private String m_name;
+        private SourceType m_source;
+        private Byte[] m_credential;
+
+        public String Name { 
+            get { return m_name; } 
+            set { m_name = value; } 
+        }
+        public SourceType Source { 
+            get { return m_source; } 
+            set { m_source = value; } 
+        }
+        public String Credential { 
+            get
+            {
+                Byte[] bytes = CrossProtect.Unprotect(m_credential, null, DataProtectionScope.CurrentUser);
+                return Encoding.UTF8.GetString(bytes);
+            }
+            set
+            {
+                Byte[] bytes = Encoding.UTF8.GetBytes(value);
+                m_credential = CrossProtect.Protect(bytes, null, DataProtectionScope.CurrentUser);
+            }
+        }
+
+        public LocationData() { }
+
+        public LocationData(SerializationInfo info, StreamingContext context)
+        {
+            m_name = (String)info.GetValue("Name", typeof(String));
+            m_source = (SourceType)info.GetValue("Source", typeof(SourceType));
+            m_credential = (Byte[]) info.GetValue("Credential", typeof(Byte[]));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", m_name, typeof(String));
+            info.AddValue("Source", m_source, typeof(SourceType));
+            info.AddValue("Credential", m_credential, typeof(Byte[]));
+        }
+    }
+}
