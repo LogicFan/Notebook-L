a9a0f39cd94d2f496cb674fdde9ad787085dc579
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-21 02:14:50 -0400

    Update LocationData.cs
    - Add Log

--- Notebook L/Notebook L/FileSystem/LocationData.cs
+++ Notebook L/Notebook L/FileSystem/LocationData.cs
@@ -3,12 +3,15 @@ using System;
 using System.Text;
 using System.Security.Cryptography;
 using System.Runtime.Serialization;
+using MetroLog;
 
 namespace Notebook_L.FileSystem
 {
     [Serializable]
     public sealed class LocationData : ISerializable
     {
+        private static readonly ILogger Log = LogManagerFactory.DefaultLogManager.GetLogger<LocationData>();
+
         public enum SourceType
         {
             Local,
@@ -19,15 +22,18 @@ namespace Notebook_L.FileSystem
         private SourceType m_source;
         private Byte[] m_credential;
 
-        public String Name { 
-            get { return m_name; } 
-            set { m_name = value; } 
+        public String Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
         }
-        public SourceType Source { 
-            get { return m_source; } 
-            set { m_source = value; } 
+        public SourceType Source
+        {
+            get { return m_source; }
+            set { m_source = value; }
         }
-        public String Credential { 
+        public String Credential
+        {
             get
             {
                 Byte[] bytes = CrossProtect.Unprotect(m_credential, null, DataProtectionScope.CurrentUser);
@@ -40,17 +46,29 @@ namespace Notebook_L.FileSystem
             }
         }
 
-        public LocationData() { }
+        public LocationData()
+        {
+            Log.Trace("Create object LocationData@{0:X8}", GetHashCode());
+        }
 
         public LocationData(SerializationInfo info, StreamingContext context)
         {
+            Log.Trace("Create object LocationData@{0:X8}", GetHashCode());
+
             m_name = (String)info.GetValue("Name", typeof(String));
             m_source = (SourceType)info.GetValue("Source", typeof(SourceType));
-            m_credential = (Byte[]) info.GetValue("Credential", typeof(Byte[]));
+            m_credential = (Byte[])info.GetValue("Credential", typeof(Byte[]));
+
+            Log.Info("Name = {0}, Source = {1}, Credential = ****",
+                Name, Source.ToString("G"));
         }
 
         public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
+            Log.Trace("LocationData@{0:X8}: GetObjectData", GetHashCode());
+            Log.Info("Name = {0}, Source = {1}, Credential = ****",
+                Name, Source.ToString("G"));
+
             info.AddValue("Name", m_name, typeof(String));
             info.AddValue("Source", m_source, typeof(SourceType));
             info.AddValue("Credential", m_credential, typeof(Byte[]));
