304ee5a3bbfd6c280b896e12a07f453d4636bb3d
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-21 02:14:35 -0400

    Update UnitTest.cs
    - Redirect log into file

--- Notebook L/UnitTest/UnitTest.cs
+++ Notebook L/UnitTest/UnitTest.cs
@@ -1,7 +1,18 @@
 ﻿
-using System;
+using MetroLog;
+using MetroLog.Targets;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
 { 
+    [TestClass]
+    public class UnitTest
+    {
+        [AssemblyInitialize()]
+        public static void Initialization(TestContext testContext)
+        {
+            LogManagerFactory.DefaultConfiguration.AddTarget(LogLevel.Trace, LogLevel.Fatal, new StreamingFileTarget());
+            GlobalCrashHandler.Configure();
+        }
+    }
 }
