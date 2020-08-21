384b2733b6c65412ad013200a3fcd889ab1327e1
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-21 01:43:26 -0400

    Update FileSystemFactory.cs
    - Implement CreateFileSystemAsync
    - Implement CreateTemporaryFolderAsync

--- Notebook L/Notebook L/FileSystem/FileSystemFactory.cs
+++ Notebook L/Notebook L/FileSystem/FileSystemFactory.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using MetroLog;
+using Notebook_L.FileSystem.Detail;
+using System;
+using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace Notebook_L.FileSystem
 {
     static public class FileSystemFactory
     {
-        public static IFileSystem CreateFileSystem(LocationData data)
+        private static readonly ILogger Log = LogManagerFactory.DefaultLogManager.GetLogger(typeof(FileSystemFactory).Name);
+
+        public static StorageFolder LocalFolder => ApplicationData.Current.LocalFolder;
+        public static StorageFolder TemporaryFolder => ApplicationData.Current.TemporaryFolder;
+        public static StorageFolder RoamingFolder => ApplicationData.Current.RoamingFolder;
+
+        public async static Task<IFileSystem> CreateFileSystemAsync(LocationData data)
         {
-            throw new NotImplementedException();
+            Log.Trace("CreateFileSystemAsync");
+
+            switch (data.Source)
+            {
+                case LocationData.SourceType.Local:
+                    return await LocalFileSystem.CreateFileSystemAsync(data);
+                default:
+                    throw new NotImplementedException();
+            }
         }
+
+        public async static Task<StorageFolder> CreateTemporaryFolderAsync()
+        {
+            Log.Trace("CreateTemporaryFolderAsync");
+
+            while (true)
+            {
+                Guid guid = Guid.NewGuid();
+                try
+                {
+                    return await TemporaryFolder.CreateFolderAsync(guid.ToString(), CreationCollisionOption.FailIfExists);
+                }
+                catch
+                {
+                    Log.Info("Nameing Collision on guid = {0}", guid.ToString());
+                }
+            }
+        }
+    }
+
+    public class NameCollisionException : Exception
+    {
+        public NameCollisionException(String msg) : base(msg) { }
     }
 }
