bde51ca73b90360e606ebbe5f3929b384c77d642
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-22 22:01:15 -0400

    Initial commit for FileItem namespace
    - Partial definition for FileItemFactory, IFileItem, Library

---
+++ Notebook L/Notebook L/FileItem/IFileItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace Notebook_L.FileItem
+{
+    public interface IFileItem : IDisposable, INotifyPropertyChanged
+    {
+        String Name { get; }
+        String Path { get; }
+        FileItemType ItemType { get; }
+
+        ImageSource Icon { get; }
+        //Boolean IsFlagged { get; }
+        
+        DateTime ModifiedTime { get; }
+
+        // TODO Add events
+        //Boolean IsOpenable { get; }
+        //Boolean IsCopyable { get; }
+        //Boolean IsMoveable { get; }
+        //Boolean IsRemoveable { get; }
+        //Boolean IsExportable { get; }
+        //Boolean IsFlagable { get; }
+
+        //EventHandler<FileItemEventArgs> RaiseOpenEvent { get; }
+        //EventHandler<FileItemEventArgs> RaiseCopyEvent { get; }
+        //EventHandler<FileItemEventArgs> RaiseMoveEvent { get; }
+        //EventHandler<FileItemEventArgs> RaiseRemoveEvent { get; }
+        //EventHandler<FileItemEventArgs> RaiseAddItemEvent { get; }
+        //EventHandler<FileItemEventArgs> RaiseExportEvent { get; }
+        //EventHandler<FileItemEventArgs> RaiseFlagEvent { get; }
+
+        //Task OpenAsync();
+        //Task CopyAsync();
+        //Task MoveAsync();
+        //Task RemoveAsync();
+        //Task AddItemAsnyc();
+        //Task ExportAsync();
+        //Task FlagAsync();
+    }
+
+    public enum FileItemType
+    {
+        Library, Folder, Document
+    }
+
+    public class FileItemEventArgs : EventArgs
+    {
+
+    }
+}
---
+++ Notebook L/Notebook L/FileItem/Library.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Xaml.Media;
+using Notebook_L.FileSystem;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Runtime.Serialization;
+
+namespace Notebook_L.FileItem
+{
+    class Library : IFileItem
+    {
+        private readonly IFileSystem m_fileSystem;
+
+        public string Name => m_fileSystem.Data.Name;
+        public string Path => m_fileSystem.RootPath;
+        public FileItemType ItemType => FileItemType.Library;
+        public ImageSource Icon { get; private set; }
+        public DateTime ModifiedTime { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Library(IFileSystem fileSystem)
+        {
+            m_fileSystem = fileSystem;
+            Icon = new SvgImageSource(new Uri("ms-appx:///Assets/" + fileSystem.Data.Source.ToString("G")));
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //[Serializable]
+    //class LibraryMetadata : ISerializable
+    //{
+        
+    //}
+}
