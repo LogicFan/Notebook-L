a62ac86f5f37cf0bc4c95c54293a84b2461370a1
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-23 06:22:44 -0400

    Update FileItemFactory.cs
    - Partial Implementation

--- Notebook L/Notebook L/FileItem/FileItemFactory.cs
+++ Notebook L/Notebook L/FileItem/FileItemFactory.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Notebook_L.FileSystem;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,20 @@ namespace Notebook_L.FileItem
 {
     public static class FileItemFactory
     {
+        private static ObservableCollection<Library> m_locations;
+        private static ObservableCollection<IFileItem> m_recentItems;
+        private static ObservableCollection<IFileItem> m_laggedItems;
+
+        public static async Task<ObservableCollection<Library>> GetLocations()
+        {
+            if (m_locations != null) { return m_locations; }
+
+            throw new NotImplementedException();
+        }
+
+        public static async Task<IFileItem> CreateFileItemAsync(IFileSystem fileSystem)
+        {
+            return await Library.CreateFileItemAsync(fileSystem);
+        }
     }
 }
