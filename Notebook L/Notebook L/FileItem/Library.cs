532a47808143180e1ca924a87154a01578bbad7d
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-08-23 06:22:32 -0400

    Update Library.cs
    - Partial implemtation

--- Notebook L/Notebook L/FileItem/Library.cs
+++ Notebook L/Notebook L/FileItem/Library.cs
@@ -4,36 +4,79 @@ using Windows.UI.Xaml.Media;
 using Notebook_L.FileSystem;
 using Windows.UI.Xaml.Media.Imaging;
 using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Notebook_L.FileItem
 {
-    class Library : IFileItem
+    public class Library : IFileItem
     {
-        private readonly IFileSystem m_fileSystem;
+        private IFileSystem m_fileSystem;
+        private ImageSource m_icon;
+        private DateTime m_modifiedTime;
+        private ObservableCollection<FileItemMetadata> m_metadata;
+        private ObservableCollection<IFileItem> m_items;
+
+        public IFileSystem UnderlyingFileSystem => m_fileSystem;
 
         public string Name => m_fileSystem.Data.Name;
         public string Path => m_fileSystem.RootPath;
         public FileItemType ItemType => FileItemType.Library;
-        public ImageSource Icon { get; private set; }
-        public DateTime ModifiedTime { get; private set; }
+        public ImageSource Icon => m_icon;
+        public DateTime ModifiedTime => m_modifiedTime;
+        public bool IsFlagged => false;
 
+        public ObservableCollection<FileItemMetadata> Metadata => m_metadata;
+        public ObservableCollection<IFileItem> Items => m_items;
+
+        public bool IsCopyable => false;
+        public bool IsMoveable => false;
+        public bool IsRemoveable => false;
+        public bool IsExportable => true;
+        public bool IsFlagable => false;
+
+        public event EventHandler<FileItemEventArgs> RaiseOpenEvent;
+        public event EventHandler<FileItemEventArgs> RaiseCopyEvent;
+        public event EventHandler<FileItemEventArgs> RaiseMoveEvent;
+        public event EventHandler<FileItemEventArgs> RaiseRemoveEvent;
+        public event EventHandler<FileItemEventArgs> RaiseExportEvent;
+        public event EventHandler<FileItemEventArgs> RaiseFlagEvent;
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public Library(IFileSystem fileSystem)
+        private Library() { }
+
+        private static async Task<ObservableCollection<FileItemMetadata>> LoadMetadata(IFileSystem fileSystem)
         {
-            m_fileSystem = fileSystem;
-            Icon = new SvgImageSource(new Uri("ms-appx:///Assets/" + fileSystem.Data.Source.ToString("G")));
+            return new ObservableCollection<FileItemMetadata>();
         }
 
-        public void Dispose()
+        public static async Task<IFileItem> CreateFileItemAsync(IFileSystem fileSystem)
+        {
+            IFileItem item = new Library()
+            {
+                m_fileSystem = fileSystem,
+                m_icon = new SvgImageSource(new Uri("ms-appx:///Assets/" + fileSystem.Data.Source.ToString("G"))),
+                m_modifiedTime = new DateTime(),
+                m_metadata = await LoadMetadata(fileSystem),
+                m_items = new ObservableCollection<IFileItem>()
+            };
+            return item;
+        }
+
+        public Task OpenAsync(object args)
         {
             throw new NotImplementedException();
         }
-    }
+        public Task ExportAsync(object args)
+        {
+            throw new NotImplementedException();
+        }
 
-    //[Serializable]
-    //class LibraryMetadata : ISerializable
-    //{
-        
-    //}
+        public Task CopyAsync(object args) { throw new InvalidOperationException(); }
+        public Task MoveAsync(object args) { throw new InvalidOperationException(); }
+        public Task RemoveAsync(object args) { throw new InvalidOperationException(); }
+        public Task FlagAsync(object args) { throw new InvalidOperationException(); }
+
+        public void Dispose() { }
+    }
 }
